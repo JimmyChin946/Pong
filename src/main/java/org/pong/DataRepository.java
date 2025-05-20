b9f72a3ebc43d627407ce3d5def8c1ff5fb234a3
JimmyChin946 <Judes946@gmail.com>
2025-05-20 13:27:56 -0700

    singleton

---
+++ src/main/java/org/pong/DataRepository.java
@@ -0,0 +1,7 @@
+package org.pong;
+
+public class DataRepository {
+
+
+
+}
--- src/main/java/org/pong/Main.java
+++ src/main/java/org/pong/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.pong;
 
 public class Main {
     public static void main(String[] args) {
