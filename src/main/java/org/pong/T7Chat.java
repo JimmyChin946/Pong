b6cc40bb1336b08ec21ab2a8fd6e9f07b804ff11
JimmyChin946 <Judes946@gmail.com>
2025-05-20 14:20:24 -0700

    chat datastructure

---
+++ src/main/java/org/pong/T7Chat.java
@@ -0,0 +1,26 @@
+package org.pong;
+
+
+/**
+* A datastructure to hold a "message"
+* this includes the name of the author
+* and the content of the message
+* 
+* TODO we can add the date if we want later
+* 
+* @author Jude Shin 
+* 
+*/
+public class Chat {
+	private String author;
+	private String content;
+	// private Date dateSent;
+
+	public Chat (String author, String content) {
+		this.author = author;
+		this.content = content;
+	}
+
+	public String getAuthor() { return author; }
+	public String getContent() { return content; }
+}
