f0a3feb5cea919d612bf7533970e4d1e259a9639
JimmyChin946 <Judes946@gmail.com>
2025-05-22 14:42:56 -0700

    Datarepository refactor

--- src/main/java/org/pong/T7DataRepository.java
+++ src/main/java/org/pong/T7DataRepository.java
@@ -2,15 +2,17 @@ package org.pong;
 
 import java.beans.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 /**
 * Singleton for all of the data used in pong
 * ball: for the location of the ball
-*	paddleA : for the location of one of the players paddle
-* paddleB : for the location of the other players paddle
-*	scoreA : the score of one of the players 
-* scoreB : the score of the other player
+*	playerHost : for the location of one of the players paddle
+* playerClient : for the location of the other players paddle
+*	scoreHost : the score of one of the players 
+* scoreClient : the score of the other player
 * chatHistory : the log of the game's chat history
 * 
 * @author Jude Shin 
@@ -21,24 +23,27 @@ public class T7DataRepository extends PropertyChangeSupport {
 
 	private T7Ball ball;
 
-	private T7Player paddleA;
-	private T7Player paddleB;
+	private T7Player playerHost;
+	private T7Player playerClient;
 
-	private int scoreA;
-	private int scoreB;
+	private int scoreHost;
+	private int scoreClient;
 
 	private ArrayList<T7Chat> chatHistory;
+
+	private Queue<PublishItem> publishQueue;
 
   private T7DataRepository() {
     super(new Object());
 		
 		// default values
 		ball = new T7Ball();
-		paddleA = new T7Player();
-		paddleB = new T7Player();
-		scoreA = 0;
-		scoreB = 0;
+		playerHost = new T7Player();
+		playerClient = new T7Player();
+		scoreHost = 0;
+		scoreClient = 0;
 		chatHistory = new ArrayList<>();
+		publishQueue = new LinkedList<>();
   }
 
   public static T7DataRepository getInstance() {
@@ -46,10 +51,22 @@ public class T7DataRepository extends PropertyChangeSupport {
     return instance;
   }
 
-	public ArrayList<T7Chat> getChatHistory() { return chatHistory; }
-	public int scoreA() { return scoreA; }
-	public int scoreB() { return scoreB; }
+	public Ball getBall() { return Ball; }
 
+	public T7Player getPlayerHost() { return playerHost; }
+	public T7Player getPlayerClient() { return playerClient; }
+
+	public int getScoreHost() { return scoreHost; }
+	public void setScoreHost(int score) { scoreHost = score; }
+
+	public int getScoreClient() { return scoreClient; }
+	public void setScoreClient(int score) { scoreClient = score; }
+
+	public ArrayList<T7Chat> getChatHistory() { return chatHistory; }
+	public void addChatHistory(T7Chat chat) { chatHistory.add(chat); }
+
+	public void pushPublishQueue(PublishItem publishItem) { publishQueue.offer(publishItem); }
+	public PublishItem popPublishQueue() { return publishQueue.poll(); }
 }
 
 
