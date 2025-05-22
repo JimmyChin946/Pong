f277c4e8beace794ffaa25ef6d275a960b3b075a
Nathan-Lackie <nlackie@calpoly.edu>
2025-05-22 14:54:02 -0700

    Update chat panel

--- src/main/java/org/pong/T7ChatPanel.java
+++ src/main/java/org/pong/T7ChatPanel.java
@@ -1,24 +1,73 @@
 package org.pong;
 
 import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
-public class T7ChatPanel extends JPanel {
-    JPanel messagePanel;
-    JTextField messageField;
+/**
+ *
+ *
+ * @author Nathan Lackie
+ */
+public class T7ChatPanel extends JPanel implements PropertyChangeListener {
+    private final int MAX_MESSAGES = 8;
+
+    private T7DataRepository repository;
+    private JPanel messagePrompt;
 
     public T7ChatPanel() {
         super();
 
-        messagePanel = new JPanel();
-        messageField = new JTextField();
+        repository = T7DataRepository.getInstance();
+        repository.addPropertyChangeListener("chat", this);
 
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        add(messagePanel);
-        add(messageField);
+        setLayout(new GridLayout(MAX_MESSAGES + 1, 1));
 
-        messagePanel.setVisible(true);
+        messagePrompt = new JPanel();
+        messagePrompt.setLayout(new GridLayout(1, 2));
+
+        JTextField messageField = new JTextField();
+        messagePrompt.add(messageField);
+        messageField.setVisible(true);
+
+        JButton messageButton = new JButton("Send");
+        messagePrompt.add(messageButton);
+        messageButton.addActionListener(e -> {
+            T7Chat message = new T7Chat("Me", messageField.getText());
+            repository.addChatHistory(message);
+        });
+        messageButton.setVisible(true);
+
+        draw(new ArrayList<>());
+
+        messagePrompt.setVisible(true);
+
         setVisible(true);
+    }
 
+    public void draw(ArrayList<T7Chat> messages) {
+        removeAll();
 
+        for (int i = messages.size() - 1; i > messages.size() - 1 - MAX_MESSAGES; i--) {
+            String line;
+            if (i > 0) {
+                line = messages.get(i).getAuthor() + messages.get(i).getContent();
+            } else {
+                line = "";
+            }
+
+            JLabel label = new JLabel(line);
+            add(label);
+            label.setVisible(true);
+        }
+
+        add(messagePrompt);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        draw((ArrayList<T7Chat>) evt.getNewValue());
     }
 }
--- src/main/java/org/pong/T7Game.java
+++ src/main/java/org/pong/T7Game.java
@@ -13,10 +13,11 @@ public class T7Game {
     public static void main(String[] args) {
         frame = new JFrame();
 
-        T7ChatHandler chatHandler = new T7ChatHandler();
-        frame.add(chatHandler.getPanel());
+        frame.add(new T7ChatPanel());
         frame.setSize(800, 600);
 
         frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
