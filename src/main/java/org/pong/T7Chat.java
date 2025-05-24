5a8b5e8ad1572206273b8346e7b39fd0abf6eb04
JimmyChin946 <Judes946@gmail.com>
2025-05-24 14:32:40 -0700

    date added to the chat datastructure

--- src/main/java/org/pong/T7Chat.java
+++ src/main/java/org/pong/T7Chat.java
@@ -1,14 +1,13 @@
 package org.pong;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 
 /**
 * A datastructure to hold a "message"
 * this includes the name of the author
 * and the content of the message
-* 
-* TODO we can add the date if we want later
 * 
 * @author Jude Shin 
 * 
@@ -18,13 +17,15 @@ public class T7Chat implements Serializable {
 
 	private String author;
 	private String content;
-	// private Date dateSent;
+	private LocalDateTime date;
 
-	public T7Chat(String author, String content) {
+	public T7Chat(String author, String content, LocalDateTime date) {
 		this.author = author;
 		this.content = content;
+		this.date = date;
 	}
 
 	public String getAuthor() { return author; }
 	public String getContent() { return content; }
+	public LocalDateTime getDate() { return date; }
 }
