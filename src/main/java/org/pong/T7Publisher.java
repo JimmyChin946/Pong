081b2a3aaa28b2963e2c8ed485108c3f4dc9e302
JimmyChin946 <Judes946@gmail.com>
2025-05-24 20:10:35 -0700

    ohmygodhowmanytimescanidothis

--- src/main/java/org/pong/T7Publisher.java
+++ src/main/java/org/pong/T7Publisher.java
@@ -64,7 +64,7 @@ public class T7Publisher implements Runnable {
 				MqttMessage message = new MqttMessage(content);
 				message.setQos(0); // {"ball", "playerHost", "playerClient"};
 				String[] priority2Topics = {"chat", "scoreClient", "scoreHost"};
-				if (!Arrays.asList(priority2Topics).contains(subTopic)) { message.setQos(2); }
+				if (Arrays.asList(priority2Topics).contains(subTopic)) { message.setQos(2); }
 
 				if (client.isConnected()) {
 					client.publish(fullTopic, message);
